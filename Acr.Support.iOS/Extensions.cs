651dff8b273c68c0cd9dc8c3ca1ef46513d132a7
Allan Ritchie <allan.ritchie@gmail.com>
2016-04-08 16:33:49 -0400

    remove useless calls and android resources

--- Acr.Support.iOS/Extensions.cs
+++ Acr.Support.iOS/Extensions.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Linq;
-using CoreGraphics;
-using Foundation;
 using UIKit;
 
 
@@ -9,27 +7,27 @@ namespace Acr.Support.iOS {
 
     public static class Extensions {
 
-        public static void Present(this UIApplication app, UIViewController controller, bool animated = true, Action action = null) {
-            if (NSThread.Current.IsMainThread)
-                PresentInternal(app, controller, animated, action);
-            else
-                app.InvokeOnMainThread(() => PresentInternal(app, controller, animated, action));
-        }
+        //public static void Present(this UIApplication app, UIViewController controller, bool animated = true, Action action = null) {
+        //    if (NSThread.Current.IsMainThread)
+        //        PresentInternal(app, controller, animated, action);
+        //    else
+        //        app.InvokeOnMainThread(() => PresentInternal(app, controller, animated, action));
+        //}
 
 
-        static void PresentInternal(UIApplication app, UIViewController controller, bool animated, Action action) {
-            var top = app.GetTopViewController();
-            if (controller.PopoverPresentationController != null) {
-                var x = top.View.Bounds.Width / 2;
-                var y = top.View.Bounds.Bottom;
-                var rect = new CGRect(x, y, 0, 0);
+        //static void PresentInternal(UIApplication app, UIViewController controller, bool animated, Action action) {
+        //    var top = app.GetTopViewController();
+        //    if (controller.PopoverPresentationController != null) {
+        //        var x = top.View.Bounds.Width / 2;
+        //        var y = top.View.Bounds.Bottom;
+        //        var rect = new CGRect(x, y, 0, 0);
 
-                controller.PopoverPresentationController.SourceView = top.View;
-                controller.PopoverPresentationController.SourceRect = rect;
-                controller.PopoverPresentationController.PermittedArrowDirections = UIPopoverArrowDirection.Unknown;
-            }
-            top.PresentViewController(controller, animated, action);
-        }
+        //        controller.PopoverPresentationController.SourceView = top.View;
+        //        controller.PopoverPresentationController.SourceRect = rect;
+        //        controller.PopoverPresentationController.PermittedArrowDirections = UIPopoverArrowDirection.Unknown;
+        //    }
+        //    top.PresentViewController(controller, animated, action);
+        //}
 
 
         public static UIWindow GetTopWindow(this UIApplication app) {
