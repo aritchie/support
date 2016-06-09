ff996131f5710eb30cb482f4ebb7936f44a30036
Allan Ritchie <allan.ritchie@gmail.com>
2016-06-08 21:01:13 -0400

    adding tvOS support

--- Acr.Support.iOS/Extensions.cs
+++ Acr.Support.iOS/Extensions.cs
@@ -3,34 +3,13 @@ using System.Linq;
 using UIKit;
 
 
-namespace Acr.Support.iOS {
+namespace Acr.Support.iOS
+{
 
-    public static class Extensions {
-
-        //public static void Present(this UIApplication app, UIViewController controller, bool animated = true, Action action = null) {
-        //    if (NSThread.Current.IsMainThread)
-        //        PresentInternal(app, controller, animated, action);
-        //    else
-        //        app.InvokeOnMainThread(() => PresentInternal(app, controller, animated, action));
-        //}
-
-
-        //static void PresentInternal(UIApplication app, UIViewController controller, bool animated, Action action) {
-        //    var top = app.GetTopViewController();
-        //    if (controller.PopoverPresentationController != null) {
-        //        var x = top.View.Bounds.Width / 2;
-        //        var y = top.View.Bounds.Bottom;
-        //        var rect = new CGRect(x, y, 0, 0);
-
-        //        controller.PopoverPresentationController.SourceView = top.View;
-        //        controller.PopoverPresentationController.SourceRect = rect;
-        //        controller.PopoverPresentationController.PermittedArrowDirections = UIPopoverArrowDirection.Unknown;
-        //    }
-        //    top.PresentViewController(controller, animated, action);
-        //}
-
-
-        public static UIWindow GetTopWindow(this UIApplication app) {
+    public static class Extensions
+    {
+        public static UIWindow GetTopWindow(this UIApplication app)
+        {
             return app
                 .Windows
                 .Reverse()
@@ -41,12 +20,14 @@ namespace Acr.Support.iOS {
         }
 
 
-        public static UIView GetTopView(this UIApplication app) {
+        public static UIView GetTopView(this UIApplication app)
+        {
             return app.GetTopWindow().Subviews.Last();
         }
 
 
-        public static UIViewController GetTopViewController(this UIApplication app) {
+        public static UIViewController GetTopViewController(this UIApplication app)
+        {
             var viewController = app.KeyWindow.RootViewController;
             while (viewController.PresentedViewController != null)
                 viewController = viewController.PresentedViewController;
---
+++ Acr.Support.tvOS/Properties/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("Acr.Support.tvOS")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Acr.Support.tvOS")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
